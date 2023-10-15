d825464a78572e8e1991d3cb5175c12b390a5789
Mathew Zummo <mlzummo@gmail.com>
2023-10-15 19:11:22 -0400

    update not not working (i think),commiting before switching to dual monitor mac
    Signed-off-by: Mathew Zummo <mlzummo@gmail.com>

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -13,6 +13,17 @@ export default {
 //   },
   setup() {
 
+    const context: Partial<OperationContext> = { // todo: abstract
+          // Use Partial<OperationContext> here
+          // headers: {
+          //   // Authorization: 'Bearer yourToken',
+          //   'x-hasura-admin-secret' : 'ViR8RukbTpJAIfgMUFgUXfOUAJt0EA4mymD8hYWzUTNByJ6LGhu5N12XXFr3sQIv' 
+          // },
+          fetchOptions: () => {
+            return { headers: { 'x-hasura-admin-secret' : 'ViR8RukbTpJAIfgMUFgUXfOUAJt0EA4mymD8hYWzUTNByJ6LGhu5N12XXFr3sQIv' } } //todo dynamic
+          }
+        }
+
     const title = ref('');
 
     const updateTodoResult = useMutation(gql`
@@ -24,11 +35,17 @@ export default {
       }
     `);
 
-
+    const updateTodo = gql`
+      mutation($id: ID!, $title: String!) {
+        update_todos(id: $id, title: $title) {
+          id
+          title
+        }
+      }
+    `;
 
     // figure out what this means:
     const AddTodo = gql`
-
       mutation($title: String!) {
         insert_todos(objects: [{ title: $title }]) {
           returning {
@@ -40,10 +57,11 @@ export default {
     `;
 
     const InsertTodo = useMutation(AddTodo);
+    const UpdateTodo = useMutation(updateTodo);
 
     return {
       updateTodo(id : any, title: any) {
-        const variables = { id, title: title || '' };
+        const variables = { id: 1 , title: title };
         updateTodoResult.executeMutation(variables).then(result => {
           // The result is almost identical to `updateTodoResult` with the exception
           // of `result.fetching` not being set and its properties not being reactive.
@@ -55,16 +73,7 @@ export default {
         console.log(title)
         console.log(title.value)
 
-        const context: Partial<OperationContext> = {
-          // Use Partial<OperationContext> here
-          // headers: {
-          //   // Authorization: 'Bearer yourToken',
-          //   'x-hasura-admin-secret' : 'ViR8RukbTpJAIfgMUFgUXfOUAJt0EA4mymD8hYWzUTNByJ6LGhu5N12XXFr3sQIv' 
-          // },
-          fetchOptions: () => {
-            return { headers: { 'x-hasura-admin-secret' : 'ViR8RukbTpJAIfgMUFgUXfOUAJt0EA4mymD8hYWzUTNByJ6LGhu5N12XXFr3sQIv' } } //todo dynamic
-          }
-        }
+
 
         const variables = { title: title.value };
       
@@ -72,8 +81,16 @@ export default {
           console.log('hjere atleastrr')
           console.log(result);
         });
+
+        const update = { id: "1", title: "this is line 85 we def updated this" };
+
+
+        UpdateTodo.executeMutation(update, context).then(result => {
+          console.log('Updating: Record 0')
+          console.log(result)
+        })
       },
-      title
+      title, 
     };
   }
 
