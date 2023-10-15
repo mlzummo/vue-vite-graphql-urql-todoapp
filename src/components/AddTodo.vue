78452c7fcd3fb296101ebfe4144cd4bdf14060ca
Mathew Zummo <mlzummo@gmail.com>
2023-10-15 17:29:34 -0400

    AddTodo (staic): working -- next is update with vars
    Signed-off-by: Mathew Zummo <mlzummo@gmail.com>

--- src/components/AddTodo.vue
+++ src/components/AddTodo.vue
@@ -1,5 +1,5 @@
 <script lang=ts>
-import { gql, useMutation } from '@urql/vue';
+import { gql, useMutation, useQuery, type OperationContext } from '@urql/vue';
 import { ref } from 'vue';
 
 import type { VueElement } from 'vue';
@@ -24,6 +24,18 @@ export default {
       }
     `);
 
+    const AddTodo = gql`
+      mutation {
+        insert_todos(objects: [{ title: "Learn GraphQL" }]) {
+          returning {
+            id
+            created_at
+          }
+        }
+      }
+    `;
+
+    const InsertTodo = useMutation(AddTodo);
 
     return {
       updateTodo(id : any, title: any) {
@@ -38,7 +50,21 @@ export default {
         e.preventDefault();
         console.log(title)
         console.log(title.value)
-        
+
+        const context: Partial<OperationContext> = {
+          // Use Partial<OperationContext> here
+          // headers: {
+          //   // Authorization: 'Bearer yourToken',
+          //   'x-hasura-admin-secret' : 'ViR8RukbTpJAIfgMUFgUXfOUAJt0EA4mymD8hYWzUTNByJ6LGhu5N12XXFr3sQIv' 
+          // },
+          fetchOptions: () => {
+            return { headers: { 'x-hasura-admin-secret' : 'ViR8RukbTpJAIfgMUFgUXfOUAJt0EA4mymD8hYWzUTNByJ6LGhu5N12XXFr3sQIv' } } //todo dynamic
+          }
+        }
+      
+        InsertTodo.executeMutation({},context).then(result => {
+          console.log('hjere atleastrr')
+        });
       },
       title
     };
